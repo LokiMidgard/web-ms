58aa0fe6390231cea566b6f1c7147c8a6d80bf8c
Patrick Kranz <patrick-kranz@live.de>
2016-01-24 15:00:40 +0100

    ...3

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,15 @@
-[string] $version = (curl https://raw.githubusercontent.com/Wyamio/Wyam/master/RELEASE).Content
+[string] $version = (curl -UseBasicParsing https://raw.githubusercontent.com/Wyamio/Wyam/master/RELEASE).Content
 $version = $version.Trim([Char]0).Trim(([char]10));
 $call = ("https://github.com/Wyamio/Wyam/releases/download/$version/Wyam-$version.zip")
 
-$tempFile = [IO.Path]::GetTempFileName()
-$tempFile = ("$tempFile.zip")
-Invoke-WebRequest $call -OutFile $tempFile
-Expand-Archive -Path $tempFile -DestinationPath ".\Wyam"
-Remove-Item $tempFile
+try{
+    $tempFile2 = [IO.Path]::GetTempFileName()
+    $tempFile = ("$tempFile2.zip")
+    Invoke-WebRequest $call -OutFile $tempFile
+    Remove-Item ".\wyam" -ErrorAction Ignore -Recurse
+    Expand-Archive -Path $tempFile -DestinationPath ".\Wyam"
+}
+finally{
+    Remove-Item $tempFile
+    Remove-Item $tempFile
+}
