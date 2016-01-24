f9e8cb383f384c3adc381f5e2497ec5de7297d81
Patrick Kranz <patrick-kranz@live.de>
2016-01-24 14:48:32 +0100

    ...1

--- deploy2.cmd
+++ deploy2.cmd
@@ -1,9 +1,1 @@
-[string] $version = (curl https://raw.githubusercontent.com/Wyamio/Wyam/master/RELEASE).Content
-$version = $version.Trim([Char]0).Trim(([char]10));
-$call = ("https://github.com/Wyamio/Wyam/releases/download/$version/Wyam-$version.zip")
-
-$tempFile = [IO.Path]::GetTempFileName()
-$tempFile = ("$tempFile.zip")
-Invoke-WebRequest $call -OutFile $tempFile
-Expand-Archive -Path $tempFile -DestinationPath ".\Wyam"
-Remove-Item $tempFile
+powershell deploy.ps1
