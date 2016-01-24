e117f7ddbea942558712fedcbcc16d12778f5479
Patrick Kranz <patrick-kranz@live.de>
2016-01-24 15:45:32 +0100

    ...

--- deploy2.cmd
+++ deploy2.cmd
@@ -1,3 +1,31 @@
+@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
+
+:: ----------------------
+:: KUDU Deployment Script
+:: Version: 1.0.2
+:: ----------------------
+
+:: Prerequisites
+:: -------------
+
+:: Verify node.js installed
+where node 2>nul >nul
+IF %ERRORLEVEL% NEQ 0 (
+  echo Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.
+  goto error
+)
+
+:: Setup
+:: -----
+
+setlocal enabledelayedexpansion
+
+SET ARTIFACTS=%~dp0%..\artifacts
+
+
+
+
+
 IF NOT DEFINED BIN_PATH (
   SET BIN_PATH=%ARTIFACTS%\..\bin
 )
