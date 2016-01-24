4b944fb9d628e4896e1caa5189bd6ee1e50ea33c
Patrick Kranz <patrick-kranz@live.de>
2016-01-24 15:17:14 +0100

    ...4

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,9 @@
-[string] $version = (curl -UseBasicParsing https://raw.githubusercontent.com/Wyamio/Wyam/master/RELEASE).Content
+[string] $version = (curl https://raw.githubusercontent.com/Wyamio/Wyam/master/RELEASE).RawContent
+$match=[Regex]::Match($version,"\r\n\r\n(?<version>[^\n\r]+)",[System.Text.RegularExpressions.RegexOptions]::Singleline );
+$version =$match.Groups["version"].Value;
 $version = $version.Trim([Char]0).Trim(([char]10));
 $call = ("https://github.com/Wyamio/Wyam/releases/download/$version/Wyam-$version.zip")
-
+echo $call
 try{
     $tempFile2 = [IO.Path]::GetTempFileName()
     $tempFile = ("$tempFile2.zip")
